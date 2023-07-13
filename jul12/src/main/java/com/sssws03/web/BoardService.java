84604438dcbb284be1de8bba2635e09730db8bae
user <user@DESKTOP-ERR5D88>
2023-07-13 17:45:44 +0900

    jul12_1

---
+++ jul12/src/main/java/com/sssws03/web/BoardService.java
@@ -0,0 +1,13 @@
+package com.sssws03.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	private BoardDAO boardDAO;
+	//Controller -> Service -> DAO -> sqlSession -> DB
+	
+}
